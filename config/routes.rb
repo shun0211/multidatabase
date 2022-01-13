2eb2ad44c957b008c97ccc7099f8ad8744a09158
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2022-01-13 21:05:31 +0900

    テスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get :health_check, to: 'health_check#health_check'
   root to: 'articles#index'
   devise_for :users
   resources :articles
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,8 @@ if [ ! -z "${AWS_SSM_AGENT_CODE}" ] && [ ! -z "${AWS_SSM_AGENT_ID}" ]; then
   amazon-ssm-agent &
 fi
 
+env
 bundle exec rails db:migrate
+echo 'rails db:migrateできている'
 bundle exec unicorn -c config/unicorn.rb
+echo 'rails db:migrateできているjk'
